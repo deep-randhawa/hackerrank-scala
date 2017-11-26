36f53f7db1fb8bc9d3a5cc0e7a941f994c632eca
Deep Randhawa <deep.randhawa@hulu.com>
2017-11-26 11:51:23 -0800

    Solved all Basic Select SQL Queries

--- src/main/sql/BasicSelect.sql
+++ src/main/sql/BasicSelect.sql
@@ -56,5 +56,71 @@ UNION
    LENGTH(CITY) AS LEN
  FROM `STATION`
  ORDER BY LEN DESC, CITY
- LIMIT 1)
+ LIMIT 1);
 
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) LIKE "A%" OR
+       UPPER(CITY) LIKE "E%" OR
+       UPPER(CITY) LIKE "I%" OR
+       UPPER(CITY) LIKE "O%" OR
+       UPPER(CITY) LIKE "U%")
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) LIKE "%A" OR
+       UPPER(CITY) LIKE "%E" OR
+       UPPER(CITY) LIKE "%I" OR
+       UPPER(CITY) LIKE "%O" OR
+       UPPER(CITY) LIKE "%U")
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) RLIKE '^[AEIOU].*[AEIOU]$')
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) NOT RLIKE '^[AEIOU].*')
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) NOT RLIKE '^.*[AEIOU]$')
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) NOT RLIKE '^[AEIOU].*[AEIOU]$')
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+SELECT DISTINCT CITY
+FROM `STATION`
+WHERE (UPPER(CITY) NOT RLIKE '^[AEIOU].*'
+       AND UPPER(CITY) NOT RLIKE '.*[AEIOU]$')
+ORDER BY CITY;
+
+-- https://www.hackerrank.com/challenges/more-than-75-marks/problem
+SELECT NAME
+FROM `STUDENTS`
+WHERE MARKS > 75
+ORDER BY SUBSTR(NAME, -3) ASC, ID ASC;
+
+-- https://www.hackerrank.com/challenges/name-of-employees/problem
+SELECT NAME
+FROM `EMPLOYEE`
+ORDER BY NAME;
+
+-- https://www.hackerrank.com/challenges/salary-of-employees/problem
+SELECT NAME
+FROM `EMPLOYEE`
+WHERE (SALARY > 2000 AND MONTHS < 10);
